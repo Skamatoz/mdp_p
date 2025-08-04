f7a0f1888be73a3ed898f411a51211765c055c0c
Skamatoz <semyonvb@gmail.com>
2025-08-04 20:40:11 +0700

    фикс 2

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+<div>redirect..</div>
+</template>
+<script setup>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  // Переход на /login сразу после рендера страницы
+  router.replace('/booking/all')
+})
+</script>
